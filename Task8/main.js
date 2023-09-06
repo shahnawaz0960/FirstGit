8d3215c8993ecae7f0b8beea82d9ae2526e826cc
shahnawaz0960 <shahnawaz0960@gmail.com>
2023-09-06 11:19:23 +0530

    Description

--- Task8/main.js
+++ Task8/main.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
         // Form Submit event
 form.addEventListener('submit', addItem);
         // Delete event
 itemList.addEventListener('click', removeItem);
+        // Filter event
+filter.addEventListener('keyup', filterItems);
 
         // Add Item
 function addItem(e) {
@@ -53,4 +56,22 @@ function removeItem(e) {
         itemList.removeChild(li);
        }
     }
+}
+
+        // Filter Items
+function filterItems(e) {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLocaleLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) !=-1) {
+        item.style.display = 'block';
+    } else {
+        item.style.display = 'none';
+    }
+});
 }
